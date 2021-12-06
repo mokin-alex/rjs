bbb4c2ab9a93c6d1cba8c923698ae6657c80a640
mokin alex <mr.aleksay@gmail.com>
2021-12-06 21:35:14 +0500

    home work 7 is not ready yet

---
+++ hw-7/mawapp7/src/components/Layout.js
@@ -0,0 +1,24 @@
+import {Link, Outlet} from "react-router-dom";
+
+export default function Layout() {
+
+    return (
+        <div>
+            <nav>
+                <h2>CHAT</h2>
+                <ul>
+                    <li>
+                        <Link to="/" >Home</Link>
+                    </li>
+                    <li>
+                        <Link to="profile">Profile</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <hr/>
+
+            <Outlet/>
+        </div>
+    );
+}
